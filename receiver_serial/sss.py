db8d067336f2fb6aa71c05464d5cd08eff904d60
emptemp <gerald.stannio.li>
2021-02-28 10:16:22 +0100

    optimized sss.py for less cpu usage

--- receiver_serial/sss.py
+++ receiver_serial/sss.py
@@ -2,13 +2,16 @@ import os,sys,datetime,time
 import serial
 import requests
 import json
+import random
 
+RECBUFFER_SIZE = 1024*4
 SS_BACKEND = 'http://127.0.0.1:5000/api/addSnapshot'
 POST_HEAD = {
               'Accept': 'application/json',
               'Accept-encoding': 'gzip, deflate',
               'Content-Type': 'application/json'  
             }
+
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 def open_serial():
@@ -27,29 +30,26 @@ def writeLog(str):
   with open("sss_log.txt", "a+") as w:
     w.write(datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + " error: " + str + "\n")
 
-
 # read serial input and create dict with data
 def read_dict():
   # busy wait read serial until newline
-  inp_str = ''
-  while('\r\n' not in inp_str):
-    bytesToRead = ser.inWaiting()
-    inp = ser.read(bytesToRead)
+  inp_str = ''   
+  while('\r\n' not in inp_str and len(inp_str) < RECBUFFER_SIZE):
+    while(not ser.inWaiting()):
+      time.sleep(0.1)      
+    inp = ser.read(ser.inWaiting())
     inp_str+=inp.decode()
   indict = dict()
   # decode into string and strip newline
-  try:
-    inp_str = inp_str.rstrip()
-    # loop trough string delimited by ;
-    for item in inp_str.split(';'):
-      # skip "PKT" and empty items
-      if(item != 'PKT' and item != ''):
-        k,v=item.split('=')
-        # add to dictionary
-        indict[k] = v
-    return(indict)
-  except:
-    writeLog("serial read")
+  inp_str = inp_str.rstrip()
+  # loop trough string delimited by ;
+  for item in inp_str.split(';'):
+    # skip "PKT" and empty items
+    if(item != 'PKT' and item != ''):
+      k,v=item.split('=')
+      # add to dictionary
+      indict[k] = v
+  return(indict)
 
 # convert temp/100 to float    
 def tempFloat(str):
@@ -64,11 +64,7 @@ def main(ser):
       request_dict['temp'] = tempFloat(input_dict.pop('TEMP'))
       request_dict['label_id'] = input_dict.pop('SRC')
       request_dict['debug'] = input_dict
-      print(request_dict['label_id'], request_dict['temp'], request_dict['debug']['RSSI'])
-      try:
-        response = requests.post(SS_BACKEND, headers=POST_HEAD, data=json.dumps(request_dict))
-      except:
-        writeLog("post request")
+      response = requests.post(SS_BACKEND, headers=POST_HEAD, data=json.dumps(request_dict))
         
 if __name__ == '__main__':
   try:
